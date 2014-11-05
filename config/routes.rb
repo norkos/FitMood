e18d4fea2f71fc4720ed280eb38253edb0592198
norkos <norbert.kosmal@gmail.com>
2014-11-05 20:42:33 +0100

    Password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'account_activations/edit'
 
   root             'static_pages#home'
@@ -14,4 +18,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+
 end
